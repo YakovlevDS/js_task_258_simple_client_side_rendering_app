52dfb88107a986edacb302bcdd999e66ca66db1d
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-02-19 07:42:28 +0200

    feat:add simple client side rendering  app

---
+++ src/data/db.js
@@ -0,0 +1,38 @@
+export const posts = [
+  {
+    id: '1',
+    title: 'Post 1',
+    text: 'Some cool text 1',
+    date: new Date().toLocaleDateString()
+  },
+  {
+    id: '2',
+    title: 'Post 2',
+    text: 'Some cool text 2',
+    date: new Date().toLocaleDateString()
+  },
+  {
+    id: '3',
+    title: 'Post 3',
+    text: 'Some cool text 3',
+    date: new Date().toLocaleDateString()
+  },
+  {
+    id: '4',
+    title: 'Post 4',
+    text: 'Some cool text 4',
+    date: new Date().toLocaleDateString()
+  },
+  {
+    id: '5',
+    title: 'Post 5',
+    text: 'Some cool text 5',
+    date: new Date().toLocaleDateString()
+  },
+  {
+    id: '6',
+    title: 'Post 6',
+    text: 'Some cool text 6',
+    date: new Date().toLocaleDateString()
+  }
+]
